89f9124faf16ee7faba6563409d3d84751bd6dbd
maheshsk61 <maheshsk61@gmail.com>
2024-08-05 21:54:11 +0530

    slideshow

--- src/Tasks/Slideshow.jsx
+++ src/Tasks/Slideshow.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from 'react'
 const slides = [
     {
-      "title": "Introduction",
-      "content": "Welcome to our presentation on the exciting world of technology!"
+        "title": "Introduction",
+        "content": "Welcome to our presentation on the exciting world of technology!"
     },
     {
-      "title": "Innovation",
-      "content": "Discover the latest breakthroughs that are shaping our future."
+        "title": "Innovation",
+        "content": "Discover the latest breakthroughs that are shaping our future."
     },
     {
-      "title": "Conclusion",
-      "content": "Thank you for joining us. Let's continue to innovate together!"
+        "title": "Conclusion",
+        "content": "Thank you for joining us. Let's continue to innovate together!"
     }
-  ];  
+];
 function Slideshow() {
     const [index, setIndex] = useState(0)
-    const button={
-        padding:'10px',
-        backgroundColor:'initial',
-        marginRight:'20px'
+    const button = {
+        padding: '10px',
+        backgroundColor: 'initial',
+        marginRight: '20px'
     }
     return (
         <center >
-            <button onClick={() => { setIndex(0) }} disabled={index===0} style={button}>Restart</button>
-            <button onClick={() => { setIndex(index - 1) }} disabled={index===0} style={button}>Prev</button>
-            <button onClick={() => { setIndex(index + 1) }} disabled={index===slides.length-1} style={button}>Next</button>
+            <button onClick={() => { setIndex(0) }} disabled={index === 0} style={button}>Restart</button>
+            <button onClick={() => { setIndex(index - 1) }} disabled={index === 0} style={button}>Prev</button>
+            <button onClick={() => { setIndex(index + 1) }} disabled={index === slides.length - 1} style={button}>Next</button>
             <h1>{slides[index].title}</h1>
             <h3>{slides[index].content}</h3>
         </center>
